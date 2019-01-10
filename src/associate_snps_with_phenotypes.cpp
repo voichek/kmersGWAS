8886aee6ee519bc3e4bf67126a7eba956929b349
Yoav  Voichek <yvoichek@burrito>
2019-01-10 10:44:03 +0100

    Now the user input MAC in addition to MAF

--- src/associate_snps_with_phenotypes.cpp
+++ src/associate_snps_with_phenotypes.cpp
@@ -3,13 +3,11 @@
 ///     @brief  Associate snps with phenotypes and output the best n snps to check with a 
 //	non approximated method
 ///
-/// Detailed description starts here.
 ///
 ///    @author  Yoav Voichek (YV), yoav.voichek@tuebingen.mpg.de
 ///
 ///  @internal
 ///    Created  11/20/18
-///   Revision  $Id: doxygen.cpp.templates,v 1.3 2010/07/06 09:20:12 mehner Exp $
 ///   Compiler  gcc/g++
 ///    Company  Max Planck Institute for Developmental Biology Dep 6
 ///  Copyright  Copyright (c) 2018, Yoav Voichek
@@ -18,7 +16,6 @@
 /// GNU General Public License as published by the Free Software Foundation.
 ///=====================================================================================
 ///
-
 
 #include "kmer_general.h"
 #include "snps_multiple_databases.h"
@@ -30,17 +27,15 @@
 #include <iterator>
 #include <utility> //std::pair
 
-
 using namespace std;
-
 int main(int argc, char* argv[])
 {
-	if(argc != 6) {
-		cerr << "usage: " << argv[0] << " <phenotypes file> <base bedbim file> <base output files> <# snps to output> <maf>" << endl;
+	if(argc != 7) {
+		cerr << "usage: " << argv[0] << 
+			" <phenotypes file> <base bedbim file> <base output files> <# snps to output> <maf> <mac>" << endl;
 		return 1;
 	}
 	// load phenotypes
-		
 	pair<vector<string>, vector<PhenotypeList> > phenotypes_info = load_phenotypes_file(argv[1]);
 	cerr << "Loading snps information" << endl;
 	MultipleSNPsDataBases snps_dataset(argv[2], phenotypes_info.second[0].first); // base bedbim, names of the phenotyped accessions
@@ -50,8 +45,10 @@ int main(int argc, char* argv[])
 	
 	double maf = atof(argv[5]); //minor allele frequency
 	cerr << "MAF = " << maf << " n_sample = " << n_samples << endl;
-	double mac = ceil(maf*n_samples); //minor allele count
-	cerr << "MAC = " << mac << endl;
+	double mac = atof(argv[6]);
+	if(mac < ceil(maf*n_samples))
+		mac = ceil(maf*n_samples); //minor allele count
+	cerr << "Minor allele count  = " << mac << endl;
 	size_t phenotype_n = phenotypes_info.first.size();
 	vector<vector<size_t> > best_snps_indices;
 	cerr << "Associating phenotypes:"; cerr.flush();
@@ -60,7 +57,6 @@ int main(int argc, char* argv[])
 		cerr << "."; cerr.flush();
 		best_snps_indices.push_back(snps_dataset.get_most_associated_snps(
 					phenotypes_info.second[phenotype_i].second,n_best_snps_to_save, mac));
-
 	}
 	double d_time = (get_time() - t0) / phenotype_n;
 	cerr << "Average time per phenotype:\t"<< d_time << endl;
